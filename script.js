9421c796988be2d324277d7b83310bd661798ae9
Hector Antonio Jaramillo Montantes <143116877+Hectorajm2001@users.noreply.github.com>
2024-05-01 23:13:16 -0600

    Create script.js
    mesnaje x

---
+++ script.js
@@ -0,0 +1,5 @@
+// Código JavaScript para agregar interactividad
+// Por ejemplo, un mensaje de saludo al cargar la página
+window.onload = function() {
+    console.log("Bienvenido a Mi Emprendimiento");
+};
